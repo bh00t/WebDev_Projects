d7e11cf3607147dc0d156c8e565e549ca2798e74
RISHABH TRIPATHI <rishabh4u@hotmail.com>
2018-10-02 21:58:21 +0530

    HelloWorld with External ReactJs

--- React/HelloWorld/Method.js
+++ React/HelloWorld/Method.js
@@ -1,9 +1,10 @@
 var hello = function tick() {
-				  const element = (
-				    <div>
-					    <center>	
-					      <h1>Hello, world!</h1>
-					      <h4>It is {new Date().toLocaleTimeString()}.</h4>
+				  
+const element = (			    
+<div>					    
+<center>						      
+<h1>Hello, world!</h1>					      
+<h4>It is {new Date().toLocaleTimeString()}.</h4>
 				      	</center>
 				   </div>
 				  );
