ce6fcbffcaeae85e96445b8a705613bc4697ca01
RISHABH TRIPATHI <rishabh4u@hotmail.com>
2018-10-02 21:41:16 +0530

    HelloWorld with external ReactJs file
    same code is working on edge but chrome is throwing error, So directly testing it from GitPages

---
+++ React/HelloWorld/Method.js
@@ -0,0 +1,13 @@
+var hello = function tick() {
+				  const element = (
+				    <div>
+					    <center>	
+					      <h1>Hello, world!</h1>
+					      <h4>It is {new Date().toLocaleTimeString()}.</h4>
+				      	</center>
+				   </div>
+				  );
+				  // highlight-next-line
+				  ReactDOM.render(element, document.getElementById('root'));
+				};
+				setInterval(hello, 999);
